7dc250d0124e2cb5a93e035ea1a4bf2a33aaf4d2
Douglas Ludlow <dludlow@cityofboise.org>
2014-04-26 08:16:13 -0600

    Added bike model and context.

---
+++ RoyalJay.BikeInventory.Data/Models/Bike.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalJay.BikeInventory.Data.Models
+{
+    public class Bike
+    {
+
+    }
+}
---
+++ RoyalJay.BikeInventory.Data/Models/InventoryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalJay.BikeInventory.Data.Models
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext()
+            : base("name=BikeInventoryContext")
+        {
+
+        }
+
+        public DbSet<Bike> Bikes { get; set; }
+    }
+}
