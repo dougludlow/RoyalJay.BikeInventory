d2943b8437f572a8768e3c0a7e4652156c45c23d
Douglas Ludlow <dludlow@cityofboise.org>
2014-04-28 08:44:54 -0600

    Lowercase route urls.

--- RoyalJay.BikeInventory.Web/App_Start/RouteConfig.cs
+++ RoyalJay.BikeInventory.Web/App_Start/RouteConfig.cs
@@ -18,6 +18,8 @@ namespace RoyalJay.BikeInventory.Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.LowercaseUrls = true;
         }
     }
 }
